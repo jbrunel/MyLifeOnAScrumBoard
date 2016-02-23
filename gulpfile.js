4f3e13ebe6ed9b2459da2d5abd0291ab758925f1
Jérôme Brunel <jb.jeromebrunel@gmail.com>
2016-02-23 21:26:36 +0100

    Added gulpfile for using livereload and basic gulp connect server

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require("gulp"),
+    connect = require("gulp-connect");
+
+gulp.task("connect", function() {
+  connect.server({
+    livereload: true
+  });
+});
+
+gulp.task("html", function() {
+  gulp.src("./app/*.html")
+    .pipe(connect.reload());
+});
+
+gulp.task("watch", function() {
+  gulp.watch(["./app/*.html"], ["html"]);
+});
+
+gulp.task("default", ["connect", "watch"]);
